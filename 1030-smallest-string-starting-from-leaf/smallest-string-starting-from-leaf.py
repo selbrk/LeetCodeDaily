dcbdb1cf1cde192825d4d5426ea34ddd5eeb91c7
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-04-17 15:03:34 +0300

    Time: 47 ms (33.96%) | Memory: 18.2 MB (7.08%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
@@ -5,20 +5,21 @@
 #         self.left = left
 #         self.right = right
 from collections import deque
-
+def to_char(value : int):
+    return chr(97 + value)
 class Solution:
     def smallestFromLeaf(self, root: TreeNode) -> str:
         umap = {i: chr(ord('a') + i) for i in range(26)}
         ans = ""
-        bfs = deque([(root, umap[root.val])])
+        bfs = deque([(root, to_char(root.val))])
         while bfs:
             current_node, current_string = bfs.popleft()
             if not (current_node.left or current_node.right) and (ans == "" or current_string < ans):
                 ans = current_string
             if current_node.left:
-                bfs.append((current_node.left, umap[current_node.left.val] + current_string))
+                bfs.append((current_node.left, to_char(current_node.left.val) + current_string))
             if current_node.right:
-                bfs.append((current_node.right, umap[current_node.right.val] + current_string))
+                bfs.append((current_node.right, to_char(current_node.right.val) + current_string))
         
         return ans
 
