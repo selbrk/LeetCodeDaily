34871cf28130cab65a4597d9cba9a1889d8d3b58
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-04-23 00:10:35 +0300

    Time: 128 ms (73.64%) | Memory: 40 MB (54.71%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    vector<vector<int>> findNeighbors(vector<int>& curr) {
-        vector<vector<int>> ans;
-        vector<int> copyv;
-        for(int i = 0; i < 4; i++) {
-            copyv = curr;
-            copyv[i]++;
-            copyv[i] %= 10;
-            ans.push_back(copyv);
-        }
-        for(int i = 0; i < 4; i++) {
-            copyv = curr;
-            copyv[i]+=9;
-            copyv[i] %= 10;
-            ans.push_back(copyv);
-        }
-        return ans;
-    }
-
     int toInt(vector<int>& curr) {
         int ans = 0;
         for(int i = 0; i < 4; i++) {
@@ -28,44 +10,48 @@ public:
     }
 
     int openLock(vector<string>& deadends, string target) {
+        int to_change[4] = {0,1,2,3}, amount[2] = {1,9};
         int n = deadends.size();
 
-        vector<vector<int>> deadendsInt(n, vector<int>(4));
-        unordered_map<int,bool> vis;
-        for(int j = 0; j < n; j++)
-            for(int i = 0; i < 4; i++)
-                deadendsInt[j][i] = deadends[j][i] - '0';
+        
+        unordered_set<int> vis, forbidden;
+        vector<int> temp;
+
+        for(auto& s : deadends){
+            temp = {s[0] -'0', s[1] -'0', s[2] -'0', s[3] -'0'};
+            forbidden.insert(toInt(temp));
+        }
         
         queue<pair<int,vector<int>>> q;
         q.push({0, {0,0,0,0}});
-        vector<int> targetArray = {target[0] - '0', target[1] - '0', target[2] -'0', target[3] - '0'};
+        vector<int> targetArray = {target[0] - '0', target[1] - '0', target[2] -'0', target[3] - '0'};        
 
-        unordered_set<int> forbidden;
-
-        for(vector<int>& x : deadendsInt)
-            forbidden.insert(toInt(x));
-        
-
-        
+        int current_level;
+        vector<int> current_state, next_state;
         while(!q.empty()) {
-            int level = q.front().first;
-            vector<int> current = q.front().second;
-            vis[toInt(current)] = true;
+            current_level = q.front().first;
+            current_state = q.front().second;
+            vis.insert(toInt(current_state));
 
             q.pop();
 
-            if(current == targetArray)
-                return level;
+            if(current_state == targetArray)
+                return current_level;
             
-            if(forbidden.count(toInt(current)))
+            if(forbidden.count(toInt(current_state)))
                 continue;
             
-            vector<vector<int>> ngbrs = findNeighbors(current);
+            for(int i = 0; i < 4; i++) {
+                for(int j = 0; j < 2; j++) {
+                    next_state = current_state;
+                    (next_state[to_change[i]] += amount[j]) %= 10;
+                    if(!vis.count(toInt(next_state))){
+                        vis.insert(toInt(next_state));
+                        q.push({current_level+1, next_state});
+                    }
 
-            for(vector<int>& ngbr : ngbrs)
-                if(!vis[toInt(ngbr)])
-                    {vis[toInt(ngbr)] = true; q.emplace(level+1, ngbr);}
-            
+                }
+            }
         }
         return -1;
     }
