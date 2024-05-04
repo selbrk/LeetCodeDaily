6c52ca40380e2ac2f5c45481a0465a34af329d6c
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-05-04 19:50:44 +0300

    Time: 57 ms (60.02%) | Memory: 45.4 MB (85.44%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
         sort(people.begin(), people.end());
-        // 1, 1, 2 ,3
-        vector<bool> vis(people.size(), false);
-        int ans = 0;
-        for (int i = 0; i < people.size(); i++) {
-            if (vis[i])
-                continue;
-            ans++;
-            vis[i] = true;
-            int left = limit - people[i];
-            // find the greatest element less than or equal to left
-            int l = i + 1, r = people.size() - 1;
-            while (l <= r) {
-                if (r - l <= 1) {
-                    if (people[r] <= left && !vis[r]) {
-                        vis[r] = true;
-                        break;
-                    }
-                    if (people[l] <= left && !vis[l]) {
-                        vis[l] = true;
-                        break;
-                    }
-                    break;
-                }
-                int mid = (l + r) / 2;
-                if (people[mid] <= left && !vis[mid])
-                    l = mid;
-                else
-                    r = mid - 1;
-            }
+        int l = 0, r = n - 1, ans = 0;
+        while(l <= r) {
+            if(people[l] + people[r] <= limit)
+                l++, r--, ans++;
+            else
+                r--, ans++;
         }
 
         return ans;
+
     }
 };
