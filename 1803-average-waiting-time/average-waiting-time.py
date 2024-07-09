9413281f00d62aa221c6b6389dd6efd37185b0ae
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-07-09 12:27:20 +0300

    Time: 788 ms (15.12%) | Memory: 57.1 MB (45.36%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.py
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -2,8 +2,7 @@ class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
         chef_end, total_wait = 0, 0
         for arrival, time in customers:
-            chef_start = max(chef_end, arrival)
-            chef_end = chef_start + time
+            chef_end = max(chef_end, arrival) + time
             total_wait += chef_end - arrival
         avg = total_wait / len(customers)
         return avg
