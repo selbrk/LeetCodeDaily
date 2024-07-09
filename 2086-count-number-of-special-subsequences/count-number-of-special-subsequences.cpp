87dd830cb1b115304cdff16430395adcc4d37b37
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-07-10 01:47:39 +0300

    Time: 159 ms (77.84%) | Memory: 174.6 MB (88.11%) - LeetSync

--- 2086-count-number-of-special-subsequences/count-number-of-special-subsequences.cpp
+++ 2086-count-number-of-special-subsequences/count-number-of-special-subsequences.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int countSpecialSubsequences(vector<int>& nums) {
-        int n = nums.size();
         int mod = 1e9 + 7;
         int a = 0, b = 0, c = 0;
 
-        for (int i = 0; i < n; i++)
-            if (nums[i] == 0)
+        for (int& x : nums)
+            if (x == 0)
                 a = (2 * a + 1) % mod;
-            else if (nums[i] == 1)
+            else if (x == 1)
                 b = ((2 * b % mod) + a) % mod;
             else
                 c = ((2 * c % mod) + b) % mod;
