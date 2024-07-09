2ed13a0c6ad87848f6f5dc75a5857b207f5a94cd
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-07-10 00:58:21 +0300

    Time: 45 ms (18.68%) | Memory: 16.7 MB (26.37%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.py
+++ 3192-maximum-xor-product/maximum-xor-product.py
@@ -3,13 +3,12 @@ class Solution:
         mod = 10**9 + 7
         flag = True
         no1, no2 = 0, 0  # no2 >= no1
-        for i in range(50, n - 1, -1):
+        for i in range(49, n - 1, -1):
             if i >= n:
-                # xor with 0
                 if (1 << i) & a:
-                    no1 += 1 << i
+                    no1 |= 1 << i
                 if (1 << i) & b:
-                    no2 += 1 << i
+                    no2 |= 1 << i
                 
         if no1 == no2:
             flag = False
@@ -18,12 +17,12 @@ class Solution:
 
         for i in range(n - 1, -1, -1):
             if (1 << i) & a == (1 << i) & b:
-                no1 += 1 << i
-                no2 += 1 << i
+                no1 |= 1 << i
+                no2 |= 1 << i
             elif flag:
-                no1 += 1 << i
+                no1 |= 1 << i
             else:
-                no2 += 1 << i
+                no2 |= 1 << i
                 flag = True
         no1 %= mod
         no2 %= mod
