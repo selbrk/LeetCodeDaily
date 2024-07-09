eb2c9cabb73ae6b60ee97d9cbeede49aa4b7c3a2
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-07-10 01:46:31 +0300

    Time: 160 ms (76.22%) | Memory: 174.6 MB (75.68%) - LeetSync

--- 2086-count-number-of-special-subsequences/count-number-of-special-subsequences.cpp
+++ 2086-count-number-of-special-subsequences/count-number-of-special-subsequences.cpp
@@ -3,23 +3,16 @@ public:
     int countSpecialSubsequences(vector<int>& nums) {
         int n = nums.size();
         int mod = 1e9 + 7;
-        vector<vector<int>> dp(n + 1, vector<int>(3));
+        int a = 0, b = 0, c = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                dp[i + 1][0] = (2 * dp[i][0] + 1) % mod;
-                dp[i + 1][1] = dp[i][1];
-                dp[i + 1][2] = dp[i][2];
-            } else if (nums[i] == 1) {
-                dp[i + 1][0] = dp[i][0];
-                dp[i + 1][1] = ((2 * dp[i][1]) % mod + dp[i][0]) % mod;
-                dp[i + 1][2] = dp[i][2];
-            } else {
-                dp[i + 1][0] = dp[i][0];
-                dp[i + 1][1] = dp[i][1];
-                dp[i + 1][2] = ((2 * dp[i][2]) % mod + dp[i][1]) % mod;
-            }
-        }
-        return dp[n][2];
+        for (int i = 0; i < n; i++)
+            if (nums[i] == 0)
+                a = (2 * a + 1) % mod;
+            else if (nums[i] == 1)
+                b = ((2 * b % mod) + a) % mod;
+            else
+                c = ((2 * c % mod) + b) % mod;
+
+        return c;
     }
 };
