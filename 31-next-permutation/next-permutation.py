c718a0df4253cc57964fd39919c197aaeae54664
Berk Sel <59570035+selbrk@users.noreply.github.com>
2024-10-09 08:56:29 +0300

    Time: 47 ms (26.12%) | Memory: 16.8 MB (8.56%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,61 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        def is_sorted(nums, start_ix):
+            for i in range(start_ix, len(nums) - 1):
+                if nums[i + 1] > nums[i]:
+                    return False
+            return True
+
+        def swap(nums, start_ix, found_ix):
+            if found_ix == -1:
+                return
+            
+            nums[start_ix], nums[found_ix] = nums[found_ix], nums[start_ix]
+        
+
+
+        def find_more(nums, start_ix):
+            if start_ix == -1: return -1
+            
+            number = nums[start_ix]
+            found_ix = -1
+            next_number = float('inf')
+
+            for i in range(len(nums) - 1, start_ix - 1, -1):
+                if next_number > nums[i] > number:
+                    found_ix = i
+                    next_number = nums[i]
+            
+            return found_ix
+            
+        def reverse(nums, start_ix):
+            l, r = start_ix, len(nums) - 1
+
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        def rec(nums, start_ix = 0):
+            if start_ix == len(nums) - 1:
+                swap(nums, len(nums) - 2, len(nums) - 1)
+                return
+            if is_sorted(nums, start_ix):
+                found_ix = find_more(nums, start_ix - 1)
+                if start_ix == 1:
+                    print(nums)
+                swap(nums, start_ix - 1, found_ix)
+                if start_ix == 1:
+                    print(nums)
+                reverse(nums, start_ix)
+                if start_ix == 1:
+                    print(nums)
+            else:
+                rec(nums, start_ix + 1)
+
+        
+        rec(nums)
