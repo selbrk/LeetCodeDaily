7797c21aeb8cc66e74c4dcba1d357d4fc8efab57
Berk Sel <59570035+selbrk@users.noreply.github.com>
2025-02-13 16:18:15 +0300

    Time: 94 ms (67.37%) | Memory: 99.1 MB (16.98%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -4,9 +4,10 @@ public:
         priority_queue<long long, vector<long long>, greater<long long> > pq;
         for(int &x:nums) pq.push((long long)x);
         int ans=0;
+        long long f,s;
         while(pq.top()<k)  {
-            long long f=pq.top(); pq.pop(); 
-            long long s=pq.top(); pq.pop();
+            f=pq.top(); pq.pop(); 
+            s=pq.top(); pq.pop();
             pq.push(s+2*f);
             ans++;
         }
