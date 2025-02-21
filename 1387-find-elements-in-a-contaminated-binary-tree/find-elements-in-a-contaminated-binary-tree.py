8318bf38a0d6985e65a5b90b05566f057112ce3b
Berk Sel <59570035+selbrk@users.noreply.github.com>
2025-02-21 22:11:37 +0300

    Time: 38 ms (14.44%) | Memory: 44.6 MB (10.70%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -8,19 +8,34 @@ class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
         self.root = root
+        self.memo = [-1] * (10**6 + 1)
+        self.memo[0] = 1
     def find(self, target: int) -> bool:
+        if self.memo[target] != -1: return bool(self.memo[target])
         s = bin(target + 1)
         s = s[s.find('1') + 1:]
 
         temp = self.root
+        current = 0
         for ch in s:
+            if self.memo[current] == 0:
+                self.memo[target] = 0
+                return False
             if ch == '1' and temp.right:
                 temp = temp.right
+                current = 2*current + 2
+                self.memo[current] = 1
             elif ch == '1':
+                self.memo[current*2 + 2] = 0
+                self.memo[target] = 0
                 return False
             elif ch == '0' and temp.left:
                 temp = temp.left
+                current = 2*current + 1
+                self.memo[current] = 1
             elif ch == '0':
+                self.memo[current*2 + 1] = 0
+                self.memo[target] = 0
                 return False
         return True
                 
